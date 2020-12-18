e8ac8c208fd74a5d4272383bdee31e136e4f7d36
Tavishi Suvarna <48873388+Tavishi10@users.noreply.github.com>
2020-12-18 14:06:41 +0530

    Add files via upload

---
+++ src/components/HeroSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './HeroSection.css'
+import '../App.css'
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <h1>A SELF-CONSCIOUS HOUSE</h1>
+            <p>Tracking its LIFECYCLE!!!</p>
+            </div>
+        
+    )
+}
+
+export default HeroSection
